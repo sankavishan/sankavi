f215ab2de9669d4294c30b81b0172be65ccbe17e
sankavishan <sankaviece123@gmail.com>
2016-09-19 20:34:04 +0530

    Update B.java

---
+++ B.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class B
+{
+public static void main(String args[])
+{
+Scanner ob=new Scanner(System.in);
+int a=ob.nextInt();
+if(a%2==0)
+{
+System.out.print("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
