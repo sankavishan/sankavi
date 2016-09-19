e83b299335ea59ff9d400c8571a5f24be2607e31
sankavishan <sankaviece123@gmail.com>
2016-09-19 21:57:10 +0530

    Update and rename A.java to Sum.java

---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Sum
+{
+public static void main(String args[])
+{
+Scanner ob=new Scanner(System.in);
+int n=ob.nextInt();
+int a,sum=0;
+for(i=0;i<n;i++)
+{
+int a=ob.nextInt();
+sum=sum+a;
+}
+System.out.print(sum);
+}
+}
