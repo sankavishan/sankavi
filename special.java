1002a8e9dc97986e8619300f0a3063e5f280c756
sankavishan <sankaviece123@gmail.com>
2016-09-19 22:10:52 +0530

    Create special.java

---
+++ special.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class special
+{
+public static void main(String args[])
+{
+Scanner ob=new Scanner(System.in);
+String a=ob.next();
+int n=a.lenght();
+while(n!=0)
+{
+ a=ob.next();
+if("a"<a>="z"&&"A"<a>"Z")
+{
+System.out.println("character");
+}
+else{
+System.out.println("symbol");
+}
+n--;
+}
+}
+}
+
